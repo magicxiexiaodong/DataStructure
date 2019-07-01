43834625c080c91a445dcd5036060edc5164459d
magicxiexiaodong <386363910@qq.com>
2019-07-01 16:59:29 +0800

    冒泡排序 优化

--- src/com/xxd/sort/BubbleSort.java
+++ src/com/xxd/sort/BubbleSort.java
@@ -1,6 +1,9 @@
 package com.xxd.sort;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
 
 /**
  * @author 38636
@@ -8,15 +11,36 @@ import java.util.Arrays;
  */
 public class BubbleSort {
 	public static void main(String[] args) {
-		int arr[] = { -1, -2, 3, 3, 10, 5 };
+	//	int arr[] = { -1, -2, 3, 3, 10, 5 };
+		
+		int arr[] = new int[80000];
+		
+		for(int i = 0 ;i<80000;i++) {
+			Random random = new  Random();
+			arr[i] = random.nextInt(80000);
+		}
+		Date currentDate = new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		String date = sdf.format(currentDate);
+		System.out.println("当前时间:"+date);
+		
+		
+		
 		bubbleSort(arr);
-		print(arr);
+		Date sortAfterDate = new Date();
+		String date2 = sdf.format(sortAfterDate);
+		System.out.println("排序后时间:"+date2);
+	//	print(arr);
+		
+		
 	}
 
 	public static void bubbleSort(int[] arr) {
+		
 		if (arr.length < 2 || arr == null) {
 			return;
 		}
+		boolean isChanged = false ;
 		for (int i = 0; i < arr.length - 1; i++) {
 			for (int j = 0; j < arr.length - 1 - i; j++) {
 				if (arr[j] > arr[j + 1]) {
@@ -28,7 +52,15 @@ public class BubbleSort {
 					arr[j] = arr[j] ^ arr[j + 1];
 					arr[j + 1] = arr[j] ^ arr[j + 1];
 					arr[j] = arr[j] ^ arr[j + 1];
+					isChanged = true;
 				}
+			}
+			/*System.out.println("第" + (i+1 ) +"次排序"  );
+			System.out.println(Arrays.toString(arr));*/
+			if( !isChanged ) {
+				break;
+			}else {
+				isChanged = false;
 			}
 		}
 
